85ba990345420fef56a23c9c19ffcd0e06687563
aveA <aveA@DESKTOP-45JM3HD>
2026-05-06 21:42:01 +0300

    +

--- script.js
+++ script.js
@@ -8,65 +8,67 @@ function openModal(project) {
       <p>Арм-ние стены, Арм-ние плиты, Вед-сть деталей, Выделить детали, Схлопнуть ВРС, Отметки отверстий,
       Сборка-группа, Стены в пилоны, Аналитика вкл-выкл, Арматура вкл-выкл, Лист с каркасом, Family manager</p>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/1SUx4PlcQ5w?si=DOzA0WrSLFvnTkI3"
-        frameborder="0" allowfullscreen>
-      </iframe>
+      <div class="video-grid">
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/1SUx4PlcQ5w?si=DOzA0WrSLFvnTkI3"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/Mj7-1_cIZzI?si=V5NFdBzl8xNBatex"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/Mj7-1_cIZzI?si=V5NFdBzl8xNBatex"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/lbDVbEKQ7dY?si=vuVdeyB8J1PfJszF"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/lbDVbEKQ7dY?si=vuVdeyB8J1PfJszF"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/DarV22jEuTA?si=GHrVR-mBlP9cv242"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/DarV22jEuTA?si=GHrVR-mBlP9cv242"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/WtocQb6Z1A4?si=HrBcwgyiGbMJ_NqS"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/WtocQb6Z1A4?si=HrBcwgyiGbMJ_NqS"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/cVwsRUdUGrc?si=uO0iRM3S9pwbLyel"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/cVwsRUdUGrc?si=uO0iRM3S9pwbLyel"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/wnXmQID0X0Y?si=BDTz3F17QZN_4knH"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/wnXmQID0X0Y?si=BDTz3F17QZN_4knH"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/G0I9TVfFbDs?si=HxpL2jYD6Z77qvx0"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/G0I9TVfFbDs?si=HxpL2jYD6Z77qvx0"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/_7vBfWcDuDQ?si=WTz3cJm7S-O5cxbQ"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/_7vBfWcDuDQ?si=WTz3cJm7S-O5cxbQ"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/OhxeALtVPak?si=h0utKnPKzBaPJ62Y"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/OhxeALtVPak?si=h0utKnPKzBaPJ62Y"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/TIEoxjjbM28?si=jYYCPtziV3Wc2jcT"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/TIEoxjjbM28?si=jYYCPtziV3Wc2jcT"
+            frameborder="0" allowfullscreen>
+          </iframe>
 
-      <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/Dw5guKAkD6A?si=jhX7z31C86lENDq4"
-        frameborder="0" allowfullscreen>
-      </iframe>
+          <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/Dw5guKAkD6A?si=jhX7z31C86lENDq4"
+            frameborder="0" allowfullscreen>
+          </iframe>
+      </div>
     `;
     }
 

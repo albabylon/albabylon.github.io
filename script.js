81450511ea01cb5acbe9efdb8f14d336c860e9c0
aveA <aveA@DESKTOP-45JM3HD>
2026-05-06 21:11:08 +0300

    +

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function openModal(project) {
       <p>Полноценное приложение для управления задачами с drag & drop.</p>
 
       <iframe width="100%" height="400"
-        src="https://www.youtube.com/embed/VIDEO_ID"
+        src="https://www.youtube.com/embed/1SUx4PlcQ5w"
         frameborder="0" allowfullscreen>
       </iframe>
     `;

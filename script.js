53584a74d879cf352bbfa891fac6ef63bbbb2edb
albabylon <barsukovvo@mail.ru>
2026-05-06 20:37:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function openModal(project) {
+    const modal = document.getElementById("modal");
+    const body = document.getElementById("modal-body");
+
+    if (project === "project1") {
+        body.innerHTML = `
+      <h2>Task Manager</h2>
+      <p>Полноценное приложение для управления задачами с drag & drop.</p>
+
+      <iframe width="100%" height="400"
+        src="https://www.youtube.com/embed/VIDEO_ID"
+        frameborder="0" allowfullscreen>
+      </iframe>
+    `;
+    }
+
+    if (project === "project2") {
+        body.innerHTML = `
+      <h2>Chat App</h2>
+      <p>Реалтайм чат с WebSocket.</p>
+
+      <img src="img1.png" style="width:100%; margin-bottom:10px;">
+      <img src="img2.png" style="width:100%;">
+    `;
+    }
+
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
